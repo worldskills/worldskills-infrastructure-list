afc68307eddd913ca3a224ed8f5a05c87f6555af
Joni Aaltonen <joni@aaltointeractive.fi>
2016-02-25 14:28:09 +0200

    fixes to standard sets

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -76,7 +76,7 @@ angular.module('ilApp')
 
     $scope.reloadEvent = function () {
       $scope.selectedEvent = $q.defer();
-
+      
       $scope.activeRole = Auth.activeRole();
       User.getEvent().then(function (result) {
         $scope.selectedEvent.resolve(result);
