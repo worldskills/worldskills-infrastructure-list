22dfbb39a022096209470e45f7e8780e6b2add33
Benjamin CHARBONNIER <benjamin.charbonnier@axiocode.com>
2017-12-05 11:08:28 +0100

    add en_us trad + config correct url

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function (grunt) {
       options: {
         port: 12200,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: 'dev-il.worldskills.org',
         livereload: 35739
       },
       livereload: {
