f6024558c43c21d2736445590dd12fec121af422
Benjamin CHARBONNIER <benjamin.charbonnier@axiocode.com>
2018-01-16 15:23:00 +0100

    #7 : Add translation + remove action

--- app/scripts/controllers/itemcategory.js
+++ app/scripts/controllers/itemcategory.js
@@ -8,7 +8,7 @@
  * Controller of the ilApp
  */
 angular.module('ilApp')
-  .controller('ItemCategoryCtrl', function ($q, $scope, $state, ItemCategory, WSAlert, APP_ROLES) {
+  .controller('ItemCategoryCtrl', function ($q, $scope, $state, $confirm, $translate, ItemCategory, WSAlert, APP_ROLES) {
 
     $scope.event = false;
     $scope.data = {};
@@ -36,4 +36,27 @@ angular.module('ilApp')
       });
     });
 
+    $scope.removeItemCategory = function (itemCategory, isCategory) {
+      var title;
+      var text;
+      if(isCategory){
+        title = $translate.instant('DELETE_ITEM_CATEGORY.TITLE');
+        text = $translate.instant('DELETE_ITEM_CATEGORY.TEXT', {text: itemCategory.name.text});
+      } else {
+        title = $translate.instant('DELETE_ITEM_SUB_CATEGORY.TITLE');
+        text = $translate.instant('DELETE_ITEM_SUB_CATEGORY.TEXT', {text: itemCategory.name.text});
+      }
+      $confirm({
+        title: title,
+        text: text,
+      }).then(function () {
+        return ItemCategory.removeItemCategory(itemCategory);
+      }).then(function (result) {
+        var data = isCategory ? $scope.data.categories : $scope.data.subCategories;
+        var index = data.indexOf(itemCategory);
+        data.splice(index, 1);
+      }).catch(function (error) {
+        WSAlert.danger(error);
+      });
+    };
   });
--- app/scripts/services/itemcategory.js
+++ app/scripts/services/itemcategory.js
@@ -8,23 +8,24 @@
  * Service in the ilApp.
  */
 angular.module('ilApp')
-  .factory('ItemCategory', function ($q, $http, API_IL) {
+  .factory('ItemCategory', function ($q, $http, API_IL, $translate) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
     var ItemCategory = { id: false, data: $q.defer(), itemCategories: {}, itemSubCategories: {}};
 
     ItemCategory.init = function(){
-        //TODO implement if needed
-        //var deferred = $q.defer();
         if(typeof ItemCategory.data.promise == 'undefined') ItemCategory.data = $q.defer();
 
-        $http.get(API_IL + "/item-category").then(function(result){
+        $http.get(API_IL + "/item-category")
+          .then(function(result){
             ItemCategory.data.resolve(result.data.connect_events);
             ItemCategory.data = result.data.connect_events;
-        },
-        function(error){
-            ItemCategory.data.reject("Could not fetch events");
-        });
+          })
+          .catch(function(error){
+            ItemCategory.data.reject($translate.instant("COULD_NOT_FETCH_ITEM_CATEGORIES"));
+          })
+        ;
+
         return ItemCategory.data.promise;
     };
 
@@ -37,12 +38,14 @@ angular.module('ilApp')
         url += "?level="+level;
       }
 
-      $http.get(url).then(function(result){
-        deferred.resolve(result.data);
-      },
-      function(error){
-        deferred.reject("Could not fetch item categories");
-      });
+      $http.get(url)
+        .then(function(result){
+          deferred.resolve(result.data);
+        })
+        .catch(function(error){
+          deferred.reject($translate.instant("COULD_NOT_FETCH_ITEM_CATEGORIES"));
+        })
+      ;
 
       return deferred.promise;
     };
@@ -56,81 +59,24 @@ angular.module('ilApp')
       return ItemCategory.getAll(eventId, 1);
     };
 
-
-    ItemCategory.getSkillsForSector = function(sectorId, eventId, status){
-        var deferred = $q.defer();
-        status = status || false;
-        $http.get(API_IL + "/events/skills/" + eventId + "/" + sectorId + "?status=" + status).then(function(result){
-            ItemCategory.skills = result.data.skills;
-            ItemCategory.id = eventId;
-            deferred.resolve(result.data.skills);
-        },
-        function(error){
-            deferred.reject("Could not fetch skills for sector");
-        });
-
-        return deferred.promise;
-    };
-
-    ItemCategory.getSkillsForEvent = function(eventId, status){
-        var deferred = $q.defer();
-      status = status || false;
-
-        $http.get(API_IL + "/events/skills/" + eventId + "?status=" + status).then(function(result){
-            ItemCategory.skills = result.data.skills;
-            ItemCategory.id = eventId;
-            deferred.resolve(result.data.skills);
-        },
-        function(error){
-            deferred.reject("Could not fetch skills for event");
-        });
-
-        return deferred.promise;
-    };
-
-    ItemCategory.getSkill = function(skillId, status){
-        var deferred = $q.defer();
-        status = status || false;
-
-        $http.get(API_IL + "/events/skill/" + skillId + "?status=" + status).then(function(result){
-            ItemCategory.id = result.data.event.id;
-            deferred.resolve(result.data);
-        },
-        function(error){
-            deferred.reject("Could not fetch skill information");
-        });
-
-        return deferred.promise;
-    };
-
-    ItemCategory.getSkillAreas = function(eventId){
+    ItemCategory.removeItemCategory = function(item){
       var deferred = $q.defer();
 
-      $http.get(API_IL + "/skill_areas/event/" + eventId).then(function(result){
-          ItemCategory.skillAreas = result.data.skill_areas;
-          deferred.resolve(ItemCategory.skillAreas);
-        },
-        function(error){
-          deferred.reject("Could not fetch skill areas: " + error);
-        });
+      $http.delete(API_IL + "/item-category/" + item.id, item)
+        .then(function(res){
+          deferred.resolve(res.data);
+        })
+        .catch(function(error){
+          var itemType = item.parent == null ? 'CATEGORY' : 'SUB_CATEGORY';
+          if(error.status === 304){
+            deferred.reject($translate.instant("COULD_NOT_REMOVE_ITEM_"+itemType+"_IN_USE"));
+          } else {
+            deferred.reject($translate.instant("COULD_NOT_REMOVE_ITEM_"+itemType, {error: error.data.user_msg}));
+          }
+        })
+      ;
 
       return deferred.promise;
-    };
-
-    ItemCategory.getParticipantCounts = function(skillId){
-      var deferred = $q.defer();
-
-      $http.get(API_IL + "/events/skill/" + skillId + "/participants").then(function(res){
-        deferred.resolve(res.data);
-      }, function(error){
-        deferred.reject("Could not get participant numbers: " + error);
-      });
-
-      return deferred.promise;
-    };
-
-    ItemCategory.getSkillManagement = function(skillId){
-      return $http.get(API_IL + "/events/skill/" + skillId + "/management");
     };
 
     return ItemCategory;
