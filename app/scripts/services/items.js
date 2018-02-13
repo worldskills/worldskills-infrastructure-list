d5f93bc0a6e08052ceb48ab24aaa9f738dacf855
Benjamin CHARBONNIER <benjamin.charbonnier@axiocode.com>
2018-02-13 15:36:51 +0100

    Add export action + loader

--- app/scripts/services/items.js
+++ app/scripts/services/items.js
@@ -10,127 +10,125 @@
 angular.module('ilApp')
   .service('Items', function ($q, $http, API_IL, ITEM_STATUS, MULTIPLIERS, Language) {
 
-   	var Items = { categories : $q.defer(), $data : $q.defer(), total: null };
+    var Items = { categories : $q.defer(), $data : $q.defer(), total: null };
 
-   	Items.getCategories = function(skillId){
-   		//if(typeof Items.categories.promise == 'undefined') Items.categories = $q.defer();
+    Items.getCategories = function(skillId){
+      //if(typeof Items.categories.promise == 'undefined') Items.categories = $q.defer();
       var deferred = $q.defer();
 
-   		$http.get(API_IL + "/categories/" + skillId).then(function(result){
-   			deferred.resolve(result.data.categories);
-   			Items.categories = result.data.categories;
-   		},
-   		function(error){
-        deferred.reject("Could not fetch categories: " + error.data.user_msg);
-   		});
+      $http.get(API_IL + "/categories/" + skillId).then(function(result){
+        deferred.resolve(result.data.categories);
+        Items.categories = result.data.categories;
+      },
+      function(error){
+      deferred.reject("Could not fetch categories: " + error.data.user_msg);
+      });
 
-   		return deferred.promise;
-   	};
+      return deferred.promise;
+    };
 
-   	Items.getItems = function(categoryId, skillId, eventId, limit, offset, filter, canceler){
-   		Items.data = $q.defer();
+    Items.getItems = function(categoryId, skillId, eventId, limit, offset, filter, canceler){
+      Items.data = $q.defer();
 
-         var filterStr = (typeof filter != 'undefined') ? "&filter=" + filter : "";
+      var filterStr = (typeof filter != 'undefined') ? "&filter=" + filter : "";
 
-   		$http.get(API_IL + "/items/" + eventId + "/skills/" + skillId + "/requested_items/" + categoryId + "?limit=" + limit + "&offset=" + offset + filterStr, {timeout: canceler.promise}).then(function(result){
-   			Items.data.resolve(result.data);
-   			Items.data = result.data;
-   		},
-   		function(error){
-          if(typeof error.data == 'undefined') Items.data.reject("Could not get items, please contact webmaster@worldskills.org");
-   		});
+      $http.get(API_IL + "/items/" + eventId + "/skills/" + skillId + "/requested_items/" + categoryId + "?limit=" + limit + "&offset=" + offset + filterStr, {timeout: canceler.promise}).then(function(result){
+        Items.data.resolve(result.data);
+        Items.data = result.data;
+      },
+      function(error){
+        if(typeof error.data == 'undefined') Items.data.reject("Could not get items, please contact webmaster@worldskills.org");
+      });
 
-   		return Items.data.promise;
-   	};
+      return Items.data.promise;
+    };
 
-      Items.saveItem = function(item, eventId, _extended){
+    Items.saveItem = function(item, eventId, _extended){
 
-        //extended view of the item in response
-         var extended = _extended || false;
+      //extended view of the item in response
+      var extended = _extended || false;
 
-         var deferred = $q.defer();
+      var deferred = $q.defer();
 
-         $http.put(API_IL + "/items/" + eventId + "/requested_items/" + item.id + "?extended=" + extended, item).then(function(result){
-            deferred.resolve(result.data);
-         },
-         function(error){
-            deferred.reject("Could not save requested item: " + error.data.user_msg);
-         });
+      $http.put(API_IL + "/items/" + eventId + "/requested_items/" + item.id + "?extended=" + extended, item).then(function(result){
+        deferred.resolve(result.data);
+      },
+      function(error){
+        deferred.reject("Could not save requested item: " + error.data.user_msg);
+      });
 
-         return deferred.promise;
-      };
+      return deferred.promise;
+    };
 
-      Items.removeItem = function(item, eventId, leaveOrphan){
-         var deferred = $q.defer();
+    Items.removeItem = function(item, eventId, leaveOrphan){
+      var deferred = $q.defer();
 
-         leaveOrphan == leaveOrphan || false;
+      leaveOrphan == leaveOrphan || false;
 
-         //deleting children automatically in API if the parent gets deleted
-         $http.delete(API_IL + "/items/" + eventId + "/requested_items/" + item.id + "?leave_orphan=" + leaveOrphan).then(function(result){
-            deferred.resolve(result.data);
-         },
-         function(error){
-            deferred.reject("Could not remove requested item: " + error.data.user_msg);
-         });
+      //deleting children automatically in API if the parent gets deleted
+      $http.delete(API_IL + "/items/" + eventId + "/requested_items/" + item.id + "?leave_orphan=" + leaveOrphan).then(function(result){
+        deferred.resolve(result.data);
+      },
+      function(error){
+        deferred.reject("Could not remove requested item: " + error.data.user_msg);
+      });
 
-         return deferred.promise;
-      };
+      return deferred.promise;
+    };
 
-      Items.linkItem = function(item, eventId){
-         var deferred = $q.defer();
+    Items.linkItem = function(item, eventId){
+      var deferred = $q.defer();
 
-         var api = API_IL + "/items/" + eventId
+      var api = API_IL + "/items/" + eventId
 
-         $http.post(api + "/requested_items/", item).then(function(result){
-            deferred.resolve(result.data);
-         },
-         function(error){
-            //delete supplied item orphan
-            deferred.reject("Could not create a requested item, please try again. " + error.data.user_msg);
-         });
+      $http.post(api + "/requested_items/", item).then(function(result){
+        deferred.resolve(result.data);
+      },
+      function(error){
+        //delete supplied item orphan
+        deferred.reject("Could not create a requested item, please try again. " + error.data.user_msg);
+      });
 
-
-         return deferred.promise;
-      };
+      return deferred.promise;
+    };
 
 
     Items.addItem = function(item, eventId, _extended){
-         var deferred = $q.defer();
+      var deferred = $q.defer();
 
-        //extended view of the item in response
-        var extended = _extended || false;
+      //extended view of the item in response
+      var extended = _extended || false;
 
-         var api = API_IL + "/items/" + eventId
-         var supplied_item = {
-            "user_generated": true, //generated by requested items view
-            "event": { id: eventId},
-            "status": { id: ITEM_STATUS.RED },
-            "description": item.description
-         };
+      var api = API_IL + "/items/" + eventId
+      var supplied_item = {
+        "user_generated": true, //generated by requested items view
+        "event": { id: eventId},
+        "status": { id: ITEM_STATUS.RED },
+        "description": item.description
+      };
 
-        //add supplied item first if needed
-        if(item.supplied_item === null){
-          $http.post(api + "/supplied_items/" + "?extended=" + extended, supplied_item).then(function(result){
-            //supplied item created
-            var new_supplied_item = result;
+      //add supplied item first if needed
+      if(item.supplied_item === null){
+        $http.post(api + "/supplied_items/" + "?extended=" + extended, supplied_item).then(function(result){
+          //supplied item created
+          var new_supplied_item = result;
 
-            //link supplied item
-            item.supplied_item = result.data;
+          //link supplied item
+          item.supplied_item = result.data;
 
-            $http.post(api + "/requested_items/", item).then(function(result){
-                deferred.resolve(result.data);
-              },
-              function(error){
-                //delete supplied item orphan
-                $http.delete(api + "/supplied_items/" + new_supplied_item.data.id);
-                deferred.reject("Could not create a requested item, please try again. " + error.data.user_msg);
-              });
-          },
-          function(error){
-            deferred.reject("Could not create a supplied item: " + error.data.user_msg);
-          });
-        }
-        else{
+          $http.post(api + "/requested_items/", item).then(function(result){
+              deferred.resolve(result.data);
+            },
+            function(error){
+              //delete supplied item orphan
+              $http.delete(api + "/supplied_items/" + new_supplied_item.data.id);
+              deferred.reject("Could not create a requested item, please try again. " + error.data.user_msg);
+            });
+        },
+        function(error){
+          deferred.reject("Could not create a supplied item: " + error.data.user_msg);
+        });
+      } else {
           //supplied item already created
           $http.post(api + "/requested_items/" + "?extended=" + extended, item).then(function(result){
               deferred.resolve(result.data);
@@ -139,36 +137,36 @@ angular.module('ilApp')
               //no need to delete supplied item orphan - as it already existed
               deferred.reject("Could not create a requested item, please try again. " + error.data.user_msg);
             });
-        }
+      }
 
-         return deferred.promise;
-      };
+      return deferred.promise;
+    };
 
-      Items.moveItem = function(eventId, skillId, itemId, parentId, index){
-         var deferred = $q.defer();
+    Items.moveItem = function(eventId, skillId, itemId, parentId, index){
+      var deferred = $q.defer();
 
-         $http.put(API_IL + "/items/" + eventId + "/requested_items/" + itemId + "/move?skill="+skillId+"&parent="+parentId+"&index="+index, {}).then(function(result){
-            deferred.resolve(result);
-         },
-         function(error){
-            deferred.reject(error.data.user_msg);
-         });
+      $http.put(API_IL + "/items/" + eventId + "/requested_items/" + itemId + "/move?skill="+skillId+"&parent="+parentId+"&index="+index, {}).then(function(result){
+        deferred.resolve(result);
+      },
+      function(error){
+        deferred.reject(error.data.user_msg);
+      });
 
-         return deferred.promise;
-      };
+      return deferred.promise;
+    };
 
-      Items.addSet = function(setId, categoryId, eventId){
-        var deferred = $q.defer();
+    Items.addSet = function(setId, categoryId, eventId){
+      var deferred = $q.defer();
 
-        $http.post(API_IL + "/items/" + eventId + "/requested_items/sets?set=" + setId + "&category=" + categoryId).then(function(res){
-          deferred.resolve(res.data);
-        },
-        function(error){
-          deferred.reject("Could not add set to the list: " + error.data.user_msg);
-        });
+      $http.post(API_IL + "/items/" + eventId + "/requested_items/sets?set=" + setId + "&category=" + categoryId).then(function(res){
+        deferred.resolve(res.data);
+      },
+      function(error){
+        deferred.reject("Could not add set to the list: " + error.data.user_msg);
+      });
 
-        return deferred.promise;
-      };
+      return deferred.promise;
+    };
 
     Items.getCatalogue = function(eventId, filters){
       var deferred = $q.defer();
@@ -211,7 +209,7 @@ angular.module('ilApp')
         air: filters.air || null,
         water: filters.water || null,
         electricity: filters.electricity || null
-      }
+      };
 
       $http.get(API_IL + '/items/' + eventId, {params: params}).then(function(result) {
         deferred.resolve(result.data);
@@ -244,6 +242,6 @@ angular.module('ilApp')
       return retval;
     };
 
-   	return Items;
+     return Items;
 
   });
