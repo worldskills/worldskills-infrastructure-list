2ecdd8ecc3f7e3a27f248e41200e54ae05dcc2ad
Fabian Vogler <fabian@equivalence.ch>
2021-04-23 11:29:05 +0200

    Add lists to event clone

--- app/scripts/controllers/event-clone.js
+++ app/scripts/controllers/event-clone.js
@@ -5,6 +5,10 @@ angular.module('ilApp').controller('EventCloneCtrl', function ($scope, $state, W
   $scope.loading = true;
   $scope.events = [];
   $scope.statuses = [];
+  $scope.lists = [];
+  $scope.allChecked = true;
+
+  var eventId = $state.params.eventId;
 
   Events.getEventsWithILs().then(function(e){
     $scope.events = e.events;
@@ -13,15 +17,29 @@ angular.module('ilApp').controller('EventCloneCtrl', function ($scope, $state, W
     WSAlert.danger($translate.instant('Unable to load events'));
   });
 
-  Status.getAllStatuses($state.params.eventId).then(function (result) {
+  Status.getAllStatuses(eventId).then(function (result) {
     $scope.statuses = result;
+  });
+
+  Events.getListsForEvent(eventId).then(function (result) {
+    $scope.lists = result;
+    angular.forEach($scope.lists, function(list){
+      list.checked = true;
+    });
   });
 
   $scope.changeEvent = function () {
 
-    Status.getAllStatuses($scope.targetEvent.id).then(function (result) {
+    var targetEventId = $scope.targetEvent.id;
+
+    Status.getAllStatuses(targetEventId).then(function (result) {
       $scope.targetStatuses = result;
     });
+
+    Events.getListsForEvent(targetEventId).then(function (result) {
+      $scope.targetLists = result;
+    });
+
   };
 
 });
