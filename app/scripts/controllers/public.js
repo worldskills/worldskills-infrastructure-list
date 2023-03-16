e52446a2a213e807150481b5b0ba9024708b7ac2
Fabian Vogler <fabian@equivalence.ch>
2023-03-16 09:56:20 +0100

    Update item.deletionSuggestions immediately

--- app/scripts/controllers/public.js
+++ app/scripts/controllers/public.js
@@ -255,6 +255,9 @@ angular.module('ilApp').controller('PublicItemsCtrl', function ($scope, $state, 
       $scope.recommendedItem.comment = modalScope.comment;
       RecommendedItems.suggestDeletion($scope.recommendedItem, $scope.eventId, $scope.listId).then(function(result) {
         WSAlert.success($translate.instant('wsalert.success.recommend_delete'));
+        if (item) {
+          item.deletionSuggestions++;
+        }
       }, function(error) {
         WSAlert.danger(error);
       });
