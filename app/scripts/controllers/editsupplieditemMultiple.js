60376f9ea915199c15beef45ced3a5106d70e5d0
Fabian Vogler <fabian@equivalence.ch>
2025-03-10 18:19:40 +1000

    Remove special handling for delivery field

--- app/scripts/controllers/editsupplieditemMultiple.js
+++ app/scripts/controllers/editsupplieditemMultiple.js
@@ -41,8 +41,6 @@ angular.module('ilApp')
         //go through all the fields and find where values are the same between every field
         angular.forEach(val, function(itemVal, itemKey){
 
-          //special handling for delivery field is no longer needed as dates are properly converted in the service
-          
           if(EXCLUDED_FIELDS.indexOf(itemKey) === -1){
             if(itemVal === "" || itemVal === null
             || (typeof $scope.item[itemKey] === 'object' && JSON.stringify($scope.item[itemKey]) !== JSON.stringify(itemVal)) //object comparison, a bit dirty but works nicely
